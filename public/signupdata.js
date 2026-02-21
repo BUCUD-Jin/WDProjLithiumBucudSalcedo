0e6ff756107988f32da406f6ba579c4d2639c9d6
BUCUD-Jin <b2029jnagbucud@pshs.edu.ph>
2026-02-21 17:50:22 +0800

    feb 21 1st commit

---
+++ public/signupdata.js
@@ -0,0 +1,25 @@
+document.querySelector('createacc').addEventListener('click', function(event) {
+    event.preventDefault();
+
+const username = document.getElementById('username').value;
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+const dimension = document.getElementById('dimension').value;
+
+const theInterests = [];
+const checkboxes = document.querySelectorAll('input[name="interest"]:checked');
+checkboxes.forEach((checkbox) => {
+    theInterests.push(checkbox.value);
+});
+
+const userData =  {
+    name: username,
+    email: email,
+    pass: password,
+    favoriteDimension: dimension,
+    interests: theInterests
+};
+
+localStorage.setItem('minecraftUser', JSON.stringify(userData));
+
+});

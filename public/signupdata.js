de93b381f90a5e5c6161e0e0a7d6c48b023fd21c
BUCUD-Jin <b2029jnagbucud@pshs.edu.ph>
2026-02-21 19:57:34 +0800

    feb 21 7th commit

--- public/signupdata.js
+++ public/signupdata.js
@@ -1,4 +1,4 @@
-document.querySelector('.createacc').addEventListener('click', function(event) {
+document.querySelector('.createacc').addEventListener('submit', function(event) {
     event.preventDefault();
 
 const username = document.getElementById('username').value;

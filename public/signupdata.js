fb2b9284953d0f291172fe59c70d7a2d160f0265
BUCUD-Jin <b2029jnagbucud@pshs.edu.ph>
2026-04-14 23:44:07 +0800

    april 14

--- public/signupdata.js
+++ public/signupdata.js
@@ -19,27 +19,22 @@ const doesEmailExist = players.some(player => player.email === email);
 if (doesUsernameExist) {
     alert("This username is already taken");
     return;
-}
-if (doesEmailExist) {
+} else if (doesEmailExist) {
     alert("This email address is already in use");
-    return;
+} else {
+    const userData = {
+        name: username,
+        email: email,
+        pass: password,
+        favoriteDimension: dimension,
+        interests: theInterests
+    };
+
+    players.push(userData);
+    localStorage.setItem('minecraftPlayers', JSON.stringify(players));
+    localStorage.setItem('minecraftUser', JSON.stringify(userData));
+    alert("Account created successfully! Welcome, " + username);
+    window.location.href = "Profile.html";
+
 }
-
-
-const userData =  {
-    name: username,
-    email: email,
-    pass: password,
-    favoriteDimension: dimension,
-    interests: theInterests
-};
-
-players.push(userData);
-localStorage.setItem('minecraftPlayers', JSON.stringify(players));
-localStorage.setItem('currentUser', JSON.stringify(userData));
-
-localStorage.setItem('minecraftUser', JSON.stringify(userData));
-alert("You've successfully created your account! Welcome, " + username);
-window.location.href = "Profile.html";
-
 });
